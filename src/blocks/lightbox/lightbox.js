f65e57f0c3a659d18dd704c7ee894b531d69c4dd
Daur Gamisonia <daurgam@gmail.com>
2018-01-17 13:24:30 +0300

    Устранены мерцания изображений Lightbox
    Устранены мерцания при смене изображений Lightbox.

--- src/blocks/lightbox/lightbox.js
+++ src/blocks/lightbox/lightbox.js
@@ -6,38 +6,44 @@ $(document).ready(function () {
 	    $lightboxInner = $(".lightbox__inner"),
 	    $lightboxImg   = $(".lightbox__img"),
 	    $lightboxClose = $(".lightbox__close"),
-	    duration       = 300;
+			duration       = 300,
+			clearDelay     = 500;
 
 	$imgLink.on("click", function (e) {
 		e.preventDefault();
 
-		$lightboxImg.attr({ src: "", alt: "" });
-
-		$('body').bind("mousewheel", function () {
-			return false;
-		}); 
-
-		$lightbox.fadeIn(duration);
-
 		var largeImage = $(this).attr("href"),
 				imageAlt   = $(this).children("img").attr("alt");
+			
+		$('body').bind("mousewheel", function () {
+			return false;
+		});
 				
 		$lightboxImg.attr({ src: largeImage, alt: imageAlt });
+		$lightbox.fadeIn(duration);
 	});
 
 	$lightboxInner.mouseenter(function () {
 		$lightbox.unbind("click");
 	});
 
+	function unloadImg() {
+		setTimeout(function() {
+			$lightboxImg.attr({ src: "", alt: "" });
+		}, clearDelay);
+	}
+
 	$lightboxInner.mouseleave(function () {
 		$lightbox.on("click", function () {
 			$('body').unbind("mousewheel");
 			$lightbox.fadeOut(duration);
+			unloadImg();
 		});
 	}),
 
 	$lightboxClose.on("click", function () {
 		$('body').unbind("mousewheel");
 		$lightbox.fadeOut(duration);
+		unloadImg();
 	})
 });
