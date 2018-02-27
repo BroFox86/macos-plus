f5ae8b12c78bd31a2fbe63eeb0218cb41a2cfb70
Daur Gamisonia <daurgam@gmail.com>
2018-02-28 00:53:53 +0300

    Обновлен скрипт главного меню

--- src/blocks/site-nav/site-nav.js
+++ src/blocks/site-nav/site-nav.js
@@ -1,17 +1,17 @@
-var $nav    = $(".site-nav__list"),
-    isFixed = "is-fixed",
-    mq      = window.matchMedia("(min-width: 60em)"),
-    offset  = $nav.offset().top,
-    margin  = $nav.outerHeight();
+var $nav       = $(".site-nav__list"),
+    fixed      = "is-fixed",
+    mediaQuery = "(min-width: 60em)",
+    offset     = $nav.offset().top,
+    margin     = $nav.outerHeight();
 
 $(window).scroll(function (event) {
   var y = $(this).scrollTop();
 
-  if (y >= offset && mq.matches) {
-    $nav.addClass(isFixed);
+  if (y >= offset && window.matchMedia(mediaQuery).matches) {
+    $nav.addClass(fixed);
     $(".header").css("margin-bottom", margin);
   } else {
-    $nav.removeClass(isFixed);
+    $nav.removeClass(fixed);
     $(".header").css("margin-bottom", "");
   }
 });
