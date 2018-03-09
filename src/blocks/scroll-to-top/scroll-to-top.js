1658967f90473f01b4ba11555e23268f31059afc
Daur Gamisonia <daurgam@gmail.com>
2018-03-09 14:15:24 +0300

    Исправлены баги с Media Query

--- src/blocks/scroll-to-top/scroll-to-top.js
+++ src/blocks/scroll-to-top/scroll-to-top.js
@@ -1,43 +1,23 @@
-function setToTopBtn() {
+var $btn    = $("button.scroll-to-top"),
+    visible = "is-visible";
 
-  // For bypass the jsdom error associated with matchMedia
-  window.matchMedia = window.matchMedia || function() {
-    return {
-        matches : false,
-        addListener : function() {},
-        removeListener: function() {}
-    };
-  };
-  
-  var $btn    = $("button.scroll-to-top"),
-      mq      = window.matchMedia("(max-width: 960px)"),
-      visible = "";
-
-  function handleMqChanges(event) {
-    visible = event.matches ? "is-visible" : "";
-  
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 800) {
-        $btn.addClass(visible);
-      } else {
-        $btn.removeClass(visible);
-      }
-    });
-  
-    $btn.click(function(e) {
-      e.preventDefault();
-      $("html, body").animate(
-        {
-          scrollTop: 0
-        },
-        2000
-      );
-      return false;
-    });
+$(window).scroll(function() {
+  if (window.matchMedia("(max-width: 960px)").matches) {
+    if ($(window).scrollTop() > 800) {
+      $btn.addClass(visible);
+    } else {
+      $btn.removeClass(visible);
+    }
   }
-  
-  mq.addListener(handleMqChanges);
-  handleMqChanges(mq);
-}
+});
 
-setToTopBtn();
+$btn.click(function(e) {
+  e.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    2000
+  );
+  return false;
+});
--- src/blocks/site-nav/site-nav.js
+++ src/blocks/site-nav/site-nav.js
@@ -1,54 +1,17 @@
-function fixSiteNav() {
-  // For bypass the jsdom error associated with matchMedia
-  window.matchMedia =
-    window.matchMedia ||
-    function() {
-      return {
-        matches: false,
-        addListener: function() {},
-        removeListener: function() {}
-      };
-    };
+var $nav     = $(".site-nav__list"),
+    $header  = $(".header"),
+    fixedNav = "is-fixed",
+    offset   = $nav.offset().top,
+    margin   = $nav.outerHeight();
 
-  var $nav   = $(".site-nav__list"),
-      mq     = window.matchMedia("(min-width: 960px)"),
-      offset = $nav.offset().top,
-      fixed  = "",
-      margin = "";
+$(window).scroll(function() {
+  var y = $(this).scrollTop();
 
-  function toggleNavFixing() {
-    $(window).scroll(function() {
-      var y = $(this).scrollTop();
-
-      if (y >= offset && mq.matches) {
-        $nav.addClass(fixed);
-        $(".header").css("margin-bottom", margin);
-      } else {
-        $nav.removeClass(fixed);
-        $(".header").css("margin-bottom", "");
-      }
-    });
+  if (y >= offset && window.matchMedia("(min-width: 960px)").matches) {
+    $nav.addClass(fixedNav);
+    $header.css("margin-bottom", margin);
+  } else {
+    $nav.removeClass(fixedNav);
+    $header.css("margin-bottom", "");
   }
-  // If IE11
-  if (
-    navigator.userAgent.match(/Trident\/7.0/) &&
-    navigator.userAgent.match(/rv:11/)
-  ) {
-    fixed = "is-fixed";
-    margin = $nav.outerHeight();
-    toggleNavFixing();
-  } 
-  
-  // Other browsers
-  else {
-    function handleMqChanges(event) {
-      fixed = event.matches ? "is-fixed" : "";
-      margin = event.matches ? $nav.outerHeight() : "";
-      toggleNavFixing();
-    }
-    mq.addListener(handleMqChanges);
-    handleMqChanges(mq);
-  }
-}
-
-fixSiteNav();
+});
