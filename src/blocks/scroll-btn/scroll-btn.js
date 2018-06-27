182e3019b59454f85973ebe5c1cac6eaa77cbc1d
Daur Gamisonia <daurgam@gmail.com>
2018-06-28 00:21:17 +0300

    Улучшен исключающий скрипт для кнопки скролла
    * Кнопка скролла не появляется на устройствах с мобильным Chrome,
    ввиду бага с фиксированными элементами на последних версиях браузера.

--- src/blocks/scroll-btn/scroll-btn.js
+++ src/blocks/scroll-btn/scroll-btn.js
@@ -5,12 +5,16 @@
       visible = "is-visible",
       media = window.matchMedia("(max-width: 960px)");
 
-  var userAgent = window.navigator.userAgent.indexOf("Chrome");
+  var userAgent = window.navigator.userAgent,
+      chrome    = userAgent.indexOf("Chrome"),
+      mobile    = userAgent.match("Android|iPhone|iPad|iPod");
 
-  $(window).on("scroll resize", function() {
-    // Turn off the scroll button due to bug in last versions of mobile Chrome
-    if ( $(window).scrollTop() > 800 && media.matches && userAgent == -1) {
-      $element.addClass(visible);
+  $(window).on("scroll resize", function () {
+    if ($(window).scrollTop() > 800 && media.matches) {
+      // Turn off the scroll button due to bug in last versions of mobile Chrome
+      if (chrome != -1 && mobile == null || chrome == -1) {
+        $element.addClass(visible);
+      }
     } else {
       $element.removeClass(visible);
     }
