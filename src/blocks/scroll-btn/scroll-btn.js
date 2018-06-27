6bf002036afab80115cc36265759d73626b26672
Daur Gamisonia <daurgam@gmail.com>
2018-06-27 20:41:22 +0300

    Обновление кнопки скролла
    * Удален лишний код
    * Временное сокрытие в браузере Chrome,
    пока не будет исправлен баг в мобильной версии, приводящий
    к странному поведению фиксированных элементов при прокручивании вверх.

--- src/blocks/scroll-btn/scroll-btn.js
+++ src/blocks/scroll-btn/scroll-btn.js
@@ -5,8 +5,11 @@
       visible = "is-visible",
       media = window.matchMedia("(max-width: 960px)");
 
+  var userAgent = window.navigator.userAgent.indexOf("Chrome");
+
   $(window).on("scroll resize", function() {
-    if ( $(window).scrollTop() > 800 && media.matches) {
+    // Turn off the scroll button due to bug in last versions of mobile Chrome
+    if ( $(window).scrollTop() > 800 && media.matches && userAgent == -1) {
       $element.addClass(visible);
     } else {
       $element.removeClass(visible);
