0dac160e8b4393f961e8aad0b14a6d40424740d7
Daur Gamisonia <daurgam@gmail.com>
2018-07-23 15:49:15 +0300

    Обновлено модальное окно
    1) Произведен рефакторинг кода и улучшено быстродействие.
    2) Упрощен функционал и удален лишний код.
    3) Сокрытие главного меню при открытии. Для устранения сдвига
    в IE.
    4) Обновлены дизайн и верстка.

--- src/blocks/lightbox/lightbox.js
+++ src/blocks/lightbox/lightbox.js
@@ -3,41 +3,48 @@
 
   var $lightbox = $(".lightbox"),
       $close    = $(".lightbox__close"),
-      duration  = 300;
+      $mainNav  = $("[data-target='lightbox']"),
+      duration  = 200;
 
+  // Add padding to body on Android browsers
+  function addPadding(padding) {
+    if (window.navigator.userAgent.match("Android") != null) {
+      $("body").css("padding-right", "0");
+    } else {
+      $("body").css("padding-right", padding);
+    }
+  };
+
+  // Open modal
   $(document).on("click", "[data-toggle='lightbox']", function(e) {
     e.preventDefault();
 
-    $lightbox.on("touchmove mousewheel", false);
+    // Add styles for properly display modal window
+    $("body").toggleClass("is-fixed");
+    addPadding("15px");
+
+    // Hide main navigation (for avoid shift on IE)
+    $mainNav.css("visibility", "hidden");
 
     var originalImage = $(this).attr("href");
 
     $lightbox
-      .children()
-      .append('<img class="lightbox__img" src="' + originalImage + '" />')
+      .find(".lightbox__inner")
+      .append('<img class="lightbox__img" src="' + originalImage + '" />');
 
     $lightbox.fadeIn(duration);
   });
 
-  $lightbox.children().mouseenter(function() {
-    $lightbox.off("click");
-  });
-
-  function unloadImg() {
-    setTimeout(function() {
-      $lightbox.find("img").remove();
-    }, duration);
-  }
-
-  $lightbox.children().mouseleave(function() {
-    $lightbox.click(function() {
-      $(this).fadeOut(duration);
-      unloadImg();
-    });
-  });
-
+  // Close modal
   $close.click(function() {
     $lightbox.fadeOut(duration);
-    unloadImg();
+
+    setTimeout(function() {
+      // Restore initial state and unload image
+      $("body").toggleClass("is-fixed");
+      addPadding(0);
+      $mainNav.css("visibility", "visible");
+      $lightbox.find("img").remove();
+    }, duration);
   });
 })();
