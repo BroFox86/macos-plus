a1cdb10b8aa81393cda69b76fffff74b00bddf70
Daur Gamisonia <daurgam@gmail.com>
2018-08-11 01:16:33 +0300

    Включена кнопка прокрутки для Chrome на Android
    В 68 версии Chrome баг с фиксированным позиционированием
    был исправлен.

--- src/blocks/scroll-btn/scroll-btn.js
+++ src/blocks/scroll-btn/scroll-btn.js
@@ -5,21 +5,9 @@
       visible = "is-visible",
       media = window.matchMedia("(max-width: 960px)");
 
-  var userAgent = window.navigator.userAgent,
-      chrome    = userAgent.match("Chrome"),
-      devices   = userAgent.match("Android|iPhone|iPad|iPod");
-
-  /* 
-     Disable the scroll button on mobile Chrome 
-     due to bug in last versions of the browser. 
-  */
-
   $(window).on("scroll resize", function () {
     if ($(window).scrollTop() > 800 && media.matches) {
-      // Disable on Chrome for Mobile
-      if (chrome != null && devices == null || chrome == null) {
-        $element.addClass(visible);
-      }
+      $element.addClass(visible);
     } else {
       $element.removeClass(visible);
     }
