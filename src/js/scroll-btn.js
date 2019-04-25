4b7d179edeaaf671b465a06bdc85ffe40344bcaa
Daur Gamisonia <daurgam@gmail.com>
2019-04-25 17:16:32 +0300

    Обновлена кнопка скролла

--- src/js/scroll-btn.js
+++ src/js/scroll-btn.js
@@ -1,36 +1,52 @@
 "use strict";
 
 /**
- * Button that scroll page to top
- * @class
- * @param {Object} options - Object with options.
+ * Scroll page to top.
+ * @param {Object} options - Options object.
  * @param {string} options.button - Button selector.
- * @param {number} options.threshold - Threshold in pixels from top of the page that show up the button.
+ * @param {number} [options.threshold=0] - Threshold in pixels from top of the page that show up the button.
+ * @param {number} [options.step=100] - Step in pixels. Larger values make animation faster.
+ * @version 3.0.0
  * @author Daur Gamisonia <daurgam@gmail.com>
  */
 function ScrollButton( options ) {
-  var btn = document.querySelector( options.button ),
-    threshold = options.threshold;
+  var button = document.querySelector( options.button );
+  var threshold = options.threshold || 0;
+  var step = (-options.step) || (-100);
+  var timeOut;
 
-  function toggle() {
-    if ( pageYOffset > threshold ) {
-      btn.classList.add("is-visible");
+  this.scrollUp = function() {
+
+    if (document.documentElement.scrollTop !=0 || document.body.scrollTop !=0) {
+
+      window.scrollBy( 0, step );
+
+      timeOut = setTimeout(function() {
+
+        this.scrollUp();
+
+      }.bind(this), 10 );
 
     } else {
-      btn.classList.remove("is-visible");
+      clearTimeout( timeOut );
     }
-  }
+  };
 
-  function toTop() {
-    scrollTo( 0, 0 );
-  }
+  function toggleVisibility() {
+    if ( pageYOffset > threshold ) {
+      button.classList.add("is-active");
+    } else {
+      button.classList.remove("is-active");
+    }
+  };
 
-  window.addEventListener( "scroll", toggle );
+  window.addEventListener( "scroll", toggleVisibility );
 
-  btn.addEventListener( "click", toTop );
+  button.onclick = this.scrollUp.bind(this);
 }
 
 var scrollButton = new ScrollButton({
-  button: "[data-toggle='scrollBtn']",
-  threshold: document.documentElement.clientHeight * 1.5
+  button: ".js-scroll-button-toggle",
+  threshold: document.documentElement.clientHeight * 1.5,
+  step: 200
 });
