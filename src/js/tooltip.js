6eb4d4f51609f776dbabafa21277ebbce1c21b11
Daur Gamisonia <daurgam@gmail.com>
2019-04-25 13:44:55 +0300

    Обновлены всплывающие подсказки

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,106 +1,77 @@
 "use strict";
 
 /**
- * Set flying tooltips.
+ * Handling flying tooltips.
  * @class
- * @version 2.0.0
+ * @version 3.0.0
  * @author Daur Gamisonia <daurgam@gmail.com>
  */
 function Tooltip() {
-  /**
-   * Tooltip element.
-   * @type {HTMLElement}
-   */
-  this._tooltip;
+  var tooltip = document.querySelector(".js-tooltip-popup");
+  var content;
+  var trigger;
+  var title;
 
-  // Add event listener at call
-  this.run();
-}
+  function handle( event ) {
 
-Tooltip.prototype._show = function( event ) {
-  var target = event.target,
-    text;
+    trigger = event.target;
 
-  if ( target.getAttribute("data-toggle") != "tooltip" ) {
-    return;
+    if ( !trigger.classList.contains("js-tooltip-trigger") ) {
+      return;
+    }
+
+    if ( tooltip.classList.contains("is-active") ) {
+
+      hide();
+
+      return;
+    }
+
+    show();
   }
 
-  if ( document.body.contains( this._tooltip ) ) {
+  function show() {
 
-    this._close( event );
+    title = trigger.title;
 
-    return;
-  }
+    trigger.title = "";
 
-  text = target.title;
+    content = document.createElement("div");
 
-  target.setAttribute( "data-text", text );
+    content.innerText = title;
 
-  target.title = "";
+    tooltip.appendChild( content );
 
-  this._tooltip = document.createElement("div");
+    tooltip.classList.add("is-active");
+  };
 
-  this._tooltip.className = "tooltip";
+  function hide() {
 
-  this._tooltip.textContent = text;
+    tooltip.classList.remove("is-active");
 
-  this._tooltip.style.left = event.pageX + 20 + "px";
+    trigger.title = content.innerText;
 
-  this._tooltip.style.top = event.pageY + 10 + "px";
+    tooltip.removeChild( content );
+  };
 
-  document.body.appendChild( this._tooltip );
+  function move( event ) {
 
-  this._tooltip.style.display = "block";
+    if ( !tooltip.classList.contains("is-active") ) {
+      return;
+    }
 
-  setTimeout(function() {
+    tooltip.style.left = event.pageX + 20 + "px";
 
-    this._tooltip.style.opacity = 1;
+    tooltip.style.top = event.pageY + 10 + "px";
+  };
 
-  }.bind(this), 30 );
-};
+  this.hide = hide;
 
-Tooltip.prototype._close = function( event ) {
-  var target = event.target,
-    text;
+  document.addEventListener( "mouseover", handle.bind(this) );
 
-  if ( !document.body.contains( this._tooltip ) ) {
-    return;
-  }
+  document.addEventListener( "mouseout", handle.bind(this) );
 
-  if ( target.getAttribute("data-toggle") != "tooltip" ) {
-    return;
-  }
-
-  text = target.getAttribute("data-text");
-
-  target.title = text;
-
-  document.body.removeChild( this._tooltip );
-};
-
-Tooltip.prototype._move = function( event ) {
-  var target = event.target;
-
-  if ( target.getAttribute("data-toggle") != "tooltip" ) {
-    return;
-  }
-
-  this._tooltip.style.left = event.pageX + 20 + "px";
-
-  this._tooltip.style.top = event.pageY + 10 + "px";
-}
-
-Tooltip.prototype.run = function() {
-  var body = document.body;
-
-  body.addEventListener( "mouseover", this._show.bind( this ) );
-
-  body.addEventListener( "mousemove", this._move.bind( this ) );
-
-  body.addEventListener( "mouseout", this._close.bind( this ) );
-
-  // For compatibility with iOS
-  body.addEventListener( "click", this._show.bind( this ) );
+  document.addEventListener( "mousemove", move.bind(this) );
 }
 
 var tooltip = new Tooltip();
